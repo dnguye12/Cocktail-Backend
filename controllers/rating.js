6fc300fe43b5a208832e3d9a9ea783fcbd230cfd
dnguye12 <duchuyng051@gmail.com>
2024-12-26 11:26:02 +0100

    edit and delete ratings endpoints

--- controllers/rating.js
+++ controllers/rating.js
@@ -2,7 +2,6 @@ const ratingRouter = require('express').Router()
 
 const Rating = require('../models/rating')
 const Cocktail = require('../models/cocktail')
-const cocktail = require('../models/cocktail')
 
 ratingRouter.get('', async (req, res) => {
     const { id } = req.query
@@ -40,9 +39,56 @@ ratingRouter.post('', async (req, res) => {
 
         await helperCocktail.save()
 
-        const returnRating = await Rating.findById(savedRating._id).populate({path: "user"})
+        const returnRating = await Rating.findById(savedRating._id).populate({ path: "user" })
 
-        return res.status(201).json(returnRating )
+        return res.status(201).json(returnRating)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json('Internal error')
+    }
+})
+
+ratingRouter.delete('/', async (req, res) => {
+    const { id } = req.query
+
+    if (!id) {
+        return res.status(400).json('Error no rating id input')
+    }
+
+    try {
+        const deletedRating = await Rating.findByIdAndDelete(id)
+
+        if (deletedRating) {
+            return res.status(200).json(deletedRating)
+        } else {
+            return res.status(404).json({ message: 'Object not found' });
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json('Internal error')
+    }
+})
+
+ratingRouter.patch('/', async (req, res) => {
+    const { id } = req.query
+    const { newContent, newRating } = req.body
+
+    if (!id) {
+        return res.status(400).json('Error no rating id input')
+    }
+
+    try {
+        const rating = await Rating.findById(id)
+
+        if (rating) {
+            rating.stars = newRating
+            rating.comment = newContent
+
+            await rating.save()
+
+            const returnRating = await Rating.findById(rating._id).populate({ path: "user" })
+            return res.status(201).json(returnRating)
+        }
     } catch (error) {
         console.log(error)
         return res.status(500).json('Internal error')
@@ -64,6 +110,30 @@ ratingRouter.get('/by-cocktail', async (req, res) => {
 
         return res.status(200).json(cocktail)
 
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json('Internal error')
+    }
+})
+
+ratingRouter.get('/user-has-rated-cocktail', async (req, res) => {
+    const { userId, cocktailId } = req.query
+
+    if (!userId || !cocktailId) {
+        return res.status(400).json('Error missing input')
+    }
+
+    try {
+        const rating = await Rating.findOne({
+            user: userId,
+            cocktail: cocktailId
+        })
+
+        if (rating) {
+            return res.status(200).json(rating)
+        } else {
+            return res.status(200).json(null)
+        }
     } catch (error) {
         console.log(error)
         return res.status(500).json('Internal error')
