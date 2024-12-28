df6d0b841a3e35f18c43001b00aa34717314cfab
dnguye12 <duchuyng051@gmail.com>
2024-12-28 08:44:29 +0100

    bubndle frontend build

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const config = require("./utils/config");
 require("express-async-errors");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 const cocktailRouter = require('./controllers/cocktail')
 const userRouter = require('./controllers/user')
@@ -30,11 +31,25 @@ mongoose
     logger.error("error connecting to MongoDB:", error.message);
   });
 
-app.use(helmet())
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      connectSrc: ["'self'", "https://pleasing-osprey-23.clerk.accounts.dev"],
+      styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com"],
+      scriptSrc: ["'self'", "https://pleasing-osprey-23.clerk.accounts.dev"],
+      workerSrc: ["'self'", "blob:"],
+      imgSrc: ["'self'", "data:", "https://img.clerk.com", "https://www.thecocktaildb.com"],
+    },
+  })
+);
 app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
+
+app.use(express.static(path.join(__dirname, "dist")));
 
 const speedLimiter = slowDown({
   windowMs: 60 * 1000,
@@ -45,6 +60,10 @@ const speedLimiter = slowDown({
 app.use('/api/cocktail', speedLimiter, cocktailRouter)
 app.use('/api/user', userRouter)
 app.use('/api/rating', ratingRouter)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
